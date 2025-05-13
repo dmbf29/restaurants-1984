23dd7e9cd1aef64a26e2ab88688b17ddd3183a72
Douglas Berkley <douglasmberkley@gmail.com>
2025-05-13 12:56:31 +0900

    before lecture day 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_13_002058) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_13_025439) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
+    t.string "category"
   end
 
 end
